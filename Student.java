74c3e874d0c421b8a96b1a6614da6dfdf7a6ed6e
Aayush Badu <baduasmit@gmail.com>
2025-01-29 21:10:26 +0545

    create a student file with name, age

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class student {
+private String name;
+private int age;
+
+student(String name, int age){
+this.name = name;
+this.age = age;
+
+public String getNAme(){
+return name;
+
+public String getAge(){
+return age;
+
+}
+}
